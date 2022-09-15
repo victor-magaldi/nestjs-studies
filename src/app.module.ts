a2ba59b7bfeaf966dac79dbd70c824e68912d053
victor de souza magaldi <victmagaldi@hotmail.com>
2022-09-15 19:55:02 -0300

    create controller customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
+  imports: [CustomersModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
